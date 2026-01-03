01a9d79d0995785169b93d06204eed11d590dcbf
zoe <azkkoo5468@gmail.com>
2026-01-03 14:25:26 +0800

    初步使用antigravity

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
-export const DEFAULT_EXPENSE_CATEGORIES = ['車用', '意外花費', '餐飲', '生活', '股票'];
+export const DEFAULT_EXPENSE_CATEGORIES = ['餐飲', '生活', '車用', '意外花費', '股票'];
 export const DEFAULT_INCOME_CATEGORIES = ['薪水', '兼職', '意外之財', '利息/回饋'];
 
 export const TARGET_SAVINGS_AMOUNT = (365 * (1 + 365)) / 2; // 66,795
